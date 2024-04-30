3803f22843ff5c88a1dd18489432f3c641762bbc
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2024-04-30 15:32:30 +0900

    device driver 구현

--- src/main/java/DeviceDriver.java
+++ src/main/java/DeviceDriver.java
@@ -1,18 +1,52 @@
+import static java.lang.Thread.sleep;
+
 /**
  * This class is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
  */
+class ReadFailException extends Exception {}
+class WriteFailException extends Exception {}
+
 public class DeviceDriver {
+    private FlashMemoryDevice hardware;
 
     public DeviceDriver(FlashMemoryDevice hardware) {
-        // TODO: implement this method
+        this.hardware = hardware;
     }
 
-    public byte read(long address) {
-        // TODO: implement this method
-        return -1;
+    public byte read(long address) throws ReadFailException{
+        byte[] readResult = new byte[5];
+        for (int i = 0; i < 5; i++) {
+            readResult[i] = hardware.read(address);
+            if (i < 4) {
+                delay200ms();
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            if (readResult[i] != readResult[i + 1]) {
+                throw new ReadFailException();
+            }
+        }
+        return readResult[0];
     }
 
-    public void write(long address, byte data) {
-        // TODO: implement this method
+    public void delay200ms() {
+        try {
+            sleep(200);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void write(long address, byte data) throws WriteFailException{
+        try {
+            if (read(address) == (byte)0xFF) {
+                hardware.write(address, data);
+            }
+            else {
+                throw new WriteFailException();
+            }
+        }
+        catch (ReadFailException e) {
+            throw new WriteFailException();
+        }
     }
 }
--- src/test/java/DeviceDriverTest.java
+++ src/test/java/DeviceDriverTest.java
@@ -1,14 +1,106 @@
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class DeviceDriverTest {
 
+    @Mock
+    FlashMemoryDevice hardware;
+
+    @Spy
+    @InjectMocks // hardware mock을 driver에 주입
+    DeviceDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        // delay 함수를 stubbing해서 read의 200ms 딜레이를 제거
+        doNothing().when(driver).delay200ms();
+    }
+
     @Test
-    public void read_From_Hardware() {
-        // TODO: replace hardware with a Test Double
-        FlashMemoryDevice hardware = null;
-        DeviceDriver driver = new DeviceDriver(hardware);
-        byte data = driver.read(0xFF);
-        assertEquals(0, data);
+    void ReadFromHardwareSuccess() {
+        when(hardware.read(0xFF))
+                .thenReturn((byte)0)
+                .thenReturn((byte)0)
+                .thenReturn((byte)0)
+                .thenReturn((byte)0)
+                .thenReturn((byte)0);
+
+        try {
+            byte data = driver.read(0xFF);
+            assertEquals(0, data);
+        }
+        catch (ReadFailException e){
+            fail();
+        }
+    }
+
+    @Test
+    void ReadFromHardwareFail() {
+        when(hardware.read(0xFF))
+                .thenReturn((byte)0)
+                .thenReturn((byte)0)
+                .thenReturn((byte)2)
+                .thenReturn((byte)0)
+                .thenReturn((byte)0);
+
+        assertThrows(ReadFailException.class, () -> {
+            driver.read(0xFF);
+        });
+    }
+
+    @Test
+    void WriteToHardwareSuccess() {
+        when(hardware.read(0xFF))
+                .thenReturn((byte)0xFF)
+                .thenReturn((byte)0xFF)
+                .thenReturn((byte)0xFF)
+                .thenReturn((byte)0xFF)
+                .thenReturn((byte)0xFF);
+
+        try {
+            driver.write(0xFF, (byte)0);
+            verify(hardware, times(1)).write(0xFF, (byte)0);
+        }
+        catch (WriteFailException e) {
+            fail();
+        }
+    }
+
+    @Test
+    void WriteToHardwareFailByNonEmptyData() {
+        when(hardware.read(0xFF))
+                .thenReturn((byte)0)
+                .thenReturn((byte)0)
+                .thenReturn((byte)0)
+                .thenReturn((byte)0)
+                .thenReturn((byte)0);
+
+        assertThrows(WriteFailException.class, () -> {
+            driver.write(0xFF, (byte)0);
+        });
+    }
+
+    @Test
+    void WriteToHardwareFailByReadFail() {
+        when(hardware.read(0xFF))
+                .thenReturn((byte)0xFF)
+                .thenReturn((byte)0xFF)
+                .thenReturn((byte)2)
+                .thenReturn((byte)0xFF)
+                .thenReturn((byte)0xFF);
+
+        assertThrows(WriteFailException.class, () -> {
+            driver.write(0xFF, (byte)0);
+        });
     }
 }
